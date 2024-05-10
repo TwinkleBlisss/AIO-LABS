5a7cb1243ffa9fdc3846ffd0c2dbe29f225634f2
Krosha31 <malkov.zhenja.ru@mail.ru>
2024-05-10 21:24:01 +0300

    fixes and changes in base class

--- lab2/algorithms/dp_weights.py
+++ lab2/algorithms/dp_weights.py
@@ -1,32 +1,34 @@
-from lab2.algorithms.base import Base
+from algorithms.base import Base
 
 
 class DP_weights(Base):
     def __init__(self, weights: list, prices: list, capacity: int):
         super().__init__(weights, prices, capacity)
-        self.dp = [[0] * (self.capacity + 1)] * len(self.weights)
-        self.items = []
+        self.dp = [[0] * (self.capacity + 1) for _ in range(len(self.weights) + 1)]
 
     def solve(self) -> None:
-        for i in range(1, len(self.dp)):
+        for i in range(len(self.dp) - 1):
             for j in range(self.capacity + 1):
                 if self.weights[i] <= j:
                     self.dp[i][j] = max(self.dp[i - 1][j], self.prices[i] + self.dp[i - 1][j - self.weights[i]])
                 else:
                     self.dp[i][j] = self.dp[i - 1][j]
+        # for i in range(len(self.dp)):
+        #     print(*self.dp[i])
         self.__find_items(len(self.weights) - 1, self.capacity)
+        items = [0] * len(self.weights)
+        for i in self.items:
+            items[i] = 1
+        self.items = items
+        self.solution_number = len(self.dp) * len(self.dp[0])
+        self.answer = self.dp[-1][-1]
 
-    def __find_items(self, pos: int, weight: int) -> None:
+    def __find_items(self, pos: int, weight: int):
         if self.dp[pos][weight] == 0:
-            return None
+            return 0
         if self.dp[pos - 1][weight] == self.dp[pos][weight]:
             self.__find_items(pos - 1, weight)
         else:
             self.__find_items(pos - 1, weight - self.weights[pos])
             self.items.append(pos)
 
-    def get_answer(self) -> int:
-        return self.dp[-1][-1]
-
-    def get_items(self) -> list:
-        return self.items
