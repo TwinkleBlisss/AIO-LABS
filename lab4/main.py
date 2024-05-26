4c98afc9bfdc8fc5a77c081d4cb1f261aee38798
Timur Yarov <yarov.tim@yandex.ru>
2024-05-27 01:38:41 +0300

    Pre-released main.

--- lab4/main.py
+++ lab4/main.py
@@ -1,12 +1,16 @@
 # TODO: write tests load like in lab3
 import os
 import time
-import numpy
+import shutil
+
+# algorithm classes
+ALGORITHMS = ["LocalSearch", "IteratedLocalSearch"]
 
 
 def tests_values(path: str = "benchmarks/") -> dict:
     values = {}
     tests_names = os.listdir(path)
+    # сортируем тесты по порядку (20, ..., 100)
     tests_names = sorted(tests_names, key=lambda test_num: int(test_num[3:-1]))
     for test_name in tests_names:
         values[test_name] = {}
@@ -35,24 +39,84 @@ def get_output_and_time(algorithm, n, D, F):
     start = time.time()
     # TODO: Класс в конструктор принимает такие параметры
     algo_class = algorithm(n, D, F)
-    algo_class.solve()
     # TODO: Тут ЕвГений добавит правильный метод вместо solve
     sol, cost = algo_class.solve()
     end = time.time()
-    return sol, cost, (end - start) * 10**3
+    return sol, cost, (end - start) * 10 ** 3
 
 
-# TODO: Функция для вывода таблицы результатов конкретного алгоритма на всех тестах
+def make_directory(path):
+    """
+    Создание ПУСТОЙ директории и переход в неё.
+    """
+    # удаляем файлы, если они есть
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+    os.mkdir(path)
+    os.chdir(path)
+
+
+def show_results(delete_answers: bool = False):
+    """
+    Функция для вывода таблицы результатов алгоритмов на всех тестах.
+    При delete_answers == True очищается папка "answers" и функция прекращает свою работу.
+    """
+    # загружаем тестовые данные
+    tests = tests_values()
+
+    # создаём директорию для ответов и ПЕРЕХОДИМ В НЕЁ
+    root = os.getcwd()  # исходная директория
+    ans_path = "answers"
+    make_directory(ans_path)
+    ans = os.getcwd()  # директория с ответами
+
+    if delete_answers:
+        return
+
+    for algorithm in ALGORITHMS:
+        # создаём директорию с ответами конкретного алгоритма и ПЕРЕХОДИМ В НЕЁ
+        os.chdir(ans)  # возвращаемся в директорию с ответами
+        make_directory(algorithm)
+
+        print(f"{algorithm}:")  # TODO: поменять на algorithm.__name__
+
+        # определяем формат заголовка
+        print('| {:^15} | {:^15} | {:^16} |'
+              .format('Test name', 'Best cost', 'Time in ms'))
+
+        # определяем формат результатов теста
+        fmt = '| {:15} | {:15} | {:4.13f} |'
+
+        for test_name, test_value in tests.items():
+            # решение алгоритма
+            # TODO: раскомментить
+            # solution, cost, algo_time = get_output_and_time(
+            #     algorithm,
+            #     test_value['n'],
+            #     test_value['D'],
+            #     test_value['F']
+            # )
+
+            # TODO: удалить
+            solution = [5, 1, 3, 4, 2]
+            cost = 666
+            algo_time = 14.88
+
+            # сохраняем solution в файлик "Testname.sol"
+            # TODO: поменять на algorithm.__name__
+            with open(f"{test_name.capitalize()}.sol", "w", encoding="utf-8") as f:
+                f.write(" ".join(map(str, solution)))
+
+            # вывод результата
+            print(fmt.format(test_name, cost, algo_time))
+        print("\n\n")
+    os.chdir(root)  # возвращаемся в исходную директорию
+
 
 def main():
     # path = "benchmarks/"
     # tests_names = os.listdir(path)
-
-
-    tests = tests_values()
-    for test_name, test_value in tests.items():
-        print(test_name)
-        print(test_value)
+    show_results(delete_answers=False)
 
 
 if __name__ == "__main__":
