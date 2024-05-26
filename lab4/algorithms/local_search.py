bad94f659ab7c482e8de8111bc1173ded25c28a9
Eugene <emmalkov@edu.hse.ru>
2024-05-27 01:44:01 +0300

    local search first version

--- lab4/algorithms/local_search.py
+++ lab4/algorithms/local_search.py
@@ -1,82 +1,26 @@
-# TODO: now it doesn't work correct
+from base import Base
+class FirstImprovement(Base):
+    def __init__(self, flows, distances, iter_num=10):
+        super().__init__(flows, distances, iter_num)
 
-import numpy as np
-
-
-def calculate_cost(D, F):
-    return np.sum(np.multiply(np.dot(F, D), F))
-
-
-def swap_elements(F, i, j):
-    F_new = np.copy(F)
-    F_new[i], F_new[j] = F_new[j], F_new[i]
-    return F_new
-
-
-def local_search(D, F, max_iterations=1000, best_improvement=True):
-    n = len(F)
-    current_cost = calculate_cost(D, F)
-    print("Посчитали поток:", current_cost)
-
-    for _ in range(max_iterations):
-        if best_improvement:
-            best_cost = current_cost
-            best_F = np.copy(F)
-
-        for i in range(n):
-            for j in range(i + 1, n):
-                F_new = swap_elements(F, i, j)
-                new_cost = calculate_cost(D, F_new)
-
-                if new_cost < current_cost:
-                    F = np.copy(F_new)
-                    current_cost = new_cost
-
-                    if not best_improvement:
-                        break
-
-        if best_improvement and best_cost < current_cost:
-            break
-
-    return F, current_cost
+    def solve(self):
+        dont_look_bits = [0] * len(self.positions)
+        for _ in range(self.iter_num):
+            for i in range(len(self.positions)):
+                if dont_look_bits[i]:
+                    continue
+                improved = False
+                for j in range(len(self.positions)):
+                    if dont_look_bits[j] or i == j:
+                        continue
+                    new_positions = self.positions.copy()
+                    new_positions[i], new_positions[j] = new_positions[j], new_positions[i]
+                    new_answer = self.get_answer(new_positions)
+                    if new_answer < self.answer:
+                        improved = True
+                        self.positions = new_positions
+                        self.answer = new_answer
+                if not improved:
+                    dont_look_bits[i] = 1
 
 
-def print_matrix(matrix: list[list[int]], matrix_name: str):
-    print(f"Matrix {matrix_name}")
-    for row in matrix:
-        print(*row)
-    print()
-
-
-# Пример использования
-n = 4
-# D = np.random.randint(1, 10, (n, n))
-# F = np.random.permutation(np.eye(n))
-D = [
-    [0, 22, 53, 0],
-    [22, 0, 40, 0],
-    [53, 40, 0, 55],
-    [0, 0, 55, 0]
-]
-F = [
-    [0, 3, 0, 2],
-    [3, 0, 0, 1],
-    [0, 0, 0, 4],
-    [2, 1, 4, 0]
-]
-# print_matrix(D, "D")
-# print_matrix(F, "F")
-
-# result_F, cost = local_search(D, F, best_improvement=False)
-# print("Матрица потока после локального поиска:")
-# print_matrix(result_F, "result_F")
-# print("Лучшая стоимость:", cost)
-print(
-    np.dot(F, D)
-)
-print(
-    np.array(F)
-)
-print(
-    np.multiply(np.dot(F, D), F)
-)
